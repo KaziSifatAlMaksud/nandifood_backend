f9f22ad4c897e5d98875fc89475a2ea44d78afbc
Kazi Sifat Al Makusd <kazi.sifat2013@gmail.com>
2025-01-29 11:21:45 +0600

    asdfasdf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,21 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->push(function ($request, $next) {
+            $response = $next($request);
+
+            // Add CORS headers to the response
+            $response->headers->set('Access-Control-Allow-Origin', '*'); // Allow all origins, you can specify a domain here
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+            // For preflight requests (OPTIONS), return a response
+            if ($request->getMethod() == 'OPTIONS') {
+                return response()->json([], 200);
+            }
+
+            return $response;
+        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
