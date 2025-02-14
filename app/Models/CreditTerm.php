6c6284193967a25b5b091e87c327b18cd1f2944a
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2025-02-14 23:19:26 +0600

    add creditTram

---
+++ app/Models/CreditTerm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CreditTerm extends Model
+{
+    use HasFactory;
+
+
+    protected $table = 'credit_terms';
+
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false; 
+
+    protected $fillable = [
+        'credit_terms',
+        'credit_type',
+        'credit_limit',
+        'credit_status',
+        'cus_sup_id',
+    ];
+
+}
