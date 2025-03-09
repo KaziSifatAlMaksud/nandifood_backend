9840a83be50e4d9a40bd0ca47064fd5e35d227c3
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2025-03-10 00:50:47 +0600

    add

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -40,8 +40,6 @@ class Product extends Model
         'last_updated',
         'last_updated_by',
         'img1',
-        'purchase_uom',
-        'production_uom',
         'upc_barcode',
         'is_approved',
         'notes'
